4cc0fec4489e4529b8184d61412670bafcd56fd6
Audri Firanti <audrifiranti@gmail>
2023-10-09 22:59:33 +0700

    audri firanti

---
+++ Tugasfundamental1/app/src/main/java/com/example/tugasfundamental1/Registeactivity.kt
@@ -0,0 +1,28 @@
+package com.example.tugasfundamental1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class Registeactivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registeactivity)
+
+        val btnRegister : Button = findViewById(R.id.btn_register)
+        btnRegister.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v.id){
+            R.id.btn_register -> {
+                val intent = Intent(this@Registeactivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
